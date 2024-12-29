48a9c93d8ca6dcd48a620b84adb8a7194bc36a7c
saim-x <x_aci@hotmail.com>
2024-12-29 19:28:29 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from langchain.document_loaders import PyPDFLoader
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+
+
+#Getting Started!!
+
+
+def main():
+    print("RAG project setup is working!")
+
+if __name__ == "__main__":
+    main()
